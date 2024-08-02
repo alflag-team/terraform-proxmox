0a50cd19799283c2c5607a868f1f95a5fe1d21e4
snake <admin@viasnake.com>
2024-08-03 03:50:39 +0900

    fix monitor1002 state

--- environments/monitor/main.tf
+++ environments/monitor/main.tf
@@ -1,16 +1,17 @@
-module "qemu" {
-  source = "../../modules/qemu"
+module "monitor1002" {
+  source = "../../modules/vm"
 
-  name         = "monitor1002"
-  target_node  = "kitsune"
-  vmid         = 0
-  clone        = "ubuntu-server-22.04"
-  cores        = 2
-  sockets      = 1
-  memory       = 1024
-  os_type      = "cloud-init"
-  disk_type    = "scsi"
-  disk_storage = "local-lvm"
-  disk_size    = "32G"
-  ipconfig0    = "ip=10.210.2.2/24,gw=10.210.0.1"
+  name        = "monitor1002"
+  target_node = "kitsune"
+  memory      = 1024 * 2
+  startup     = "order=2,up=60,down=60"
+  ipconfig0   = "ip=10.210.2.2/24,gw=10.210.0.1"
+  storage     = "local-lvm"
+  cores       = 2
+  size        = "32G"
+  vmid        = 101
+  full_clone  = false
+  cicustom    = "user=local:snippets/user-data.yaml"
+  ciuser      = var.ciuser
+  cipassword  = var.cipassword
 }

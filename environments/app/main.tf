32c1726bf227d9560b7684dcae74001832bb46d4
snake <admin@viasnake.com>
2024-08-22 00:12:15 +0900

    add wiki1001

--- environments/app/main.tf
+++ environments/app/main.tf
@@ -10,3 +10,16 @@ module "dify" {
   rootfs_storage = "Synology-01-LUN-01"
   nesting     = true
 }
+
+module "wiki1001" {
+  source = "../../modules/lxc"
+
+  hostname    = "wiki1001"
+  target_node = "kitsune"
+  memory      = 1024 * 2
+  swap        = 1024 * 8
+  network_ip  = "10.210.11.10/24"
+  rootfs_size    = "30G"
+  rootfs_storage = "Synology-01-LUN-01"
+  nesting     = true
+}

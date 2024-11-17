d0a274bb9838af6b5553c6a6bac0c77b67e251ec
snake <admin@viasnake.com>
2024-11-17 15:20:38 +0900

    update dify1001

--- environments/app/main.tf
+++ environments/app/main.tf
@@ -1,12 +1,12 @@
-module "dify" {
+module "dify1001" {
   source = "../../modules/lxc"
 
   hostname       = "dify"
   target_node    = "kitsune"
   memory         = 1024 * 4
   swap           = 1024 * 8
-  network_ip     = "10.210.11.3/24"
-  rootfs_size    = "10G"
+  network_ip     = "10.210.2.3/24"
+  rootfs_size    = "30G"
   rootfs_storage = "Synology-01-LUN-01"
   nesting        = true
 }
